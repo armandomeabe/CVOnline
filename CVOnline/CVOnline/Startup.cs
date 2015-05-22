2387d74daa462ab3cd1372b0da9932e3ce00afbd
Armando Andrés Meabe <armando.meabe@outlook.com>
2015-05-22 02:07:56 -0300

    Commit inicial del proyecto, there we go!

---
+++ CVOnline/CVOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVOnline.Startup))]
+namespace CVOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
